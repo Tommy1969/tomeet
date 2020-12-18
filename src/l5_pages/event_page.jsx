829a3dc4c19720da9fccfff54f7355abbd04c3b6
Tommy109 <tokutomi@degino.com>
2020-12-18 20:26:43 +0900

    refactor: 地図コンポーネントを切り出した。

--- src/l5_pages/event_page.jsx
+++ src/l5_pages/event_page.jsx
@@ -1,29 +1,6 @@
-import styled from 'styled-components'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {EventMap} from '../l3_organisms/event_map'
 import {EventEntry} from '../l3_organisms/event_entry'
 import {EventList} from '../l3_organisms/event_list'
-
-const Div = styled.div`
-  > div {
-    height: 40vh;
-  }
-`
-
-const EventMap = () =>
-  <Div>
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} data-testid="HOGEHOGE">
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  </Div>
-
 
 export const EventPage = ({id, title, place, events, ...props}) =>
   <>
