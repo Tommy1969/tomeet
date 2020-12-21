05d461c103bd8538e565df9cd6ccdb9785092e14
Tommy109 <tokutomi@degino.com>
2020-12-21 20:32:29 +0900

    feat: 現在地を取得できるようにした。

--- src/l3_organisms/event_map.jsx
+++ src/l3_organisms/event_map.jsx
@@ -1,6 +1,24 @@
-import 'leaflet/dist/leaflet.css'
+import {atom, useRecoilState} from 'recoil'
 import styled from 'styled-components'
+import "leaflet/dist/leaflet.css"
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet'
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetinaUrl,
+  iconUrl: iconUrl,
+  shadowUrl: shadowUrl
+})
+
+const positionState = atom({
+  key:        'positionState',
+  default:    [0, 0],
+  persistence_UNSTABLE: {type: 'log'}
+})
 
 const Div = styled.div`
   > div {
@@ -8,7 +26,20 @@ const Div = styled.div`
   }
 `
 
+/**
+ * 位置をズラすための乱数を取得する
+ */
+function getMask() {
+  const min = -0.03
+  const max = 0.03
+  return Math.random() * (max - min) + min;
+}
+
 export const EventMap = ({position, ...props}) => {
+  const [current, setCurrent] = useRecoilState(positionState)
+  navigator.geolocation.getCurrentPosition(pos => {
+    setCurrent([pos.coords.latitude + getMask(), pos.coords.longitude + getMask()])
+  })
   function MyComponent() {
     useMapEvent('click', (e) => {
       const latlng = [e.latlng.lat, e.latlng.lng]
@@ -27,9 +58,9 @@ export const EventMap = ({position, ...props}) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+          <Popup>イベント開催地</Popup>
+        </Marker>
+        <Marker position={current}>
         </Marker>
       </MapContainer>
     </Div>
